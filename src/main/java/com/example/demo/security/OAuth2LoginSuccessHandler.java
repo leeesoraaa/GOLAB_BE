699712f51fca86b1df5e8b555bb0347c8fa451bb
jacktheripper9705 <136789283+jacktheripper9705@users.noreply.github.com>
2024-07-30 14:34:01 +0900

    Update OAuth2LoginSuccessHandler.java

--- src/main/java/com/example/demo/security/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/example/demo/security/OAuth2LoginSuccessHandler.java
@@ -57,6 +57,6 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write("{\"token\": \"" + jwtToken + "\"}");
-        response.sendRedirect("http://localhost:3000");
+        response.sendRedirect("http://43.203.223.215:3000");
     }
 }
